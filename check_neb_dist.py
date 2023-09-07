bba9e5e26d4bdea04fa7f566ebac8d465de7c0b6
finthon <734513887@qq.com>
2023-09-07 14:46:28 +0800

    Update check_neb_dist.py

--- check_neb_dist.py
+++ check_neb_dist.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 -------------------------------------------------
-   File Name：     check_neb_dist
+   File Name：     test
    Description :
    Author :       DrZ
-   date：          2023/6/5
+   date：          2023/9/7
 -------------------------------------------------
    Change Activity:
-                   2023/6/5:
+                   2023/9/7:
 -------------------------------------------------
 """
-import re
 import sys
 import numpy as np
+from vaspy.atomco import PosCar
+
 
 """
 check IS and FS atom index and revised CONTCAR(FS) consistent with IS.
@@ -21,63 +22,80 @@ e.g. if move_atoms index in ase.gui is 0 3 4;   check_neb_dist CONTCAR CONTCAR 0
 tips: CONTCAR need absolute path
 """
 
+def expand_coord(point, aa, bb):
+    tran1 = point + aa
+    tran2 = point + aa + bb
+    tran3 = point + aa - bb
+    tran4 = point - aa
+    tran5 = point - aa + bb
+    tran6 = point - aa - bb
+    tran7 = point + bb
+    tran8 = point - bb
+    point_sum = [point, tran1, tran2, tran3, tran4, tran5, tran6, tran7, tran8]
+    return point_sum
 
-def read_contcar(contcar):
-    file_is = open(contcar, 'r')
-    data = file_is.readlines()
-    file_is.close()
+
+def get_index(CON_IS, CON_FS, move_list):
+    """
+    :param CON_IS: CONTCAR_IS
+    :param CON_FS: CONTCAR_FS
+    :param move_list: NEB move atoms
+    :return: an index list with the correct sort
+    """
+    pos_is = PosCar(CON_IS)
+    pos_fs = PosCar(CON_FS)
+    pos_is_data = pos_is.data
+    pos_fs_data = pos_fs.data
+    aa = pos_is.bases[0]
+    bb = pos_is.bases[1]
+    # convert to cartesian
+    cart_is = pos_is.dir2cart(pos_is.bases, pos_is_data)
+    cart_fs = pos_fs.dir2cart(pos_fs.bases, pos_fs_data)
+    # convert to n*3 array, due to only one atom with 3*1 shape.
+    if cart_is.shape == (3,):
+        cart_is = cart_is.reshape(-1, 3)
+    if cart_fs.shape == (3,):
+        cart_fs = cart_fs.reshape(-1, 3)
+
+    # expand all FS coord
+    all_FS_coord = []
+    for ccc in cart_fs:
+        all_FS_coord.extend(expand_coord(ccc, aa, bb))
+
+    index_list = []
+    # screen every coord in IS
+    for num, coord in enumerate(cart_is):
+        #print('atom: {} start'.format(num))
+        if num in move_list:
+            index_list.append(num)
+        else:
+            all_dist = []
+            coord_sum = expand_coord(coord, aa, bb)
+            for point_one in coord_sum:
+                for point_two in all_FS_coord:
+                    dist = np.linalg.norm(point_one - point_two)
+                    all_dist.append(dist)
+            ind = np.argsort(all_dist)[0] % 1323 # 取余
+            ind = ind // 9  # 取整
+            index_list.append(ind)
+    return index_list
+
+
+def generate_CONT(CON_FS, index):
+    # generate New CONTCAR
+    file_fs = open(CON_FS, 'r')
+    data = file_fs.readlines()
+    file_fs.close()
     # header
     qian_zhui = data[:9]
-    # atom numbers
-    num = data[6]
-    a = re.split('\s+', num.strip())
-    num = sum([int(i) for i in a])
-    # coord array
-    coord = data[9:9 + num]
-    coord_list = []
-    for i in coord:
-        a = re.split('\s+', i.strip())
-        coord_list.append([float(a[0]), float(a[1]), float(a[2])])
-    coord_array = np.array(coord_list)
-    return coord_array, num, qian_zhui
-
-
-def compare_data(is_file, fs_file, ts_atoms, threshold_value=0.2):
-    IS, num, qian_zhui = read_contcar(is_file)
-    FS, num, qian_zhui = read_contcar(fs_file)
-    dist_array = np.zeros((num, 1))
-    for nu in range(num):
-        dist_nu = np.linalg.norm(IS[nu] - FS[nu])
-        dist_array[nu] = dist_nu
-
-    condition = np.sum(dist_array > threshold_value)
-    if condition == len(ts_atoms):
-        print('IS and FS consistent')
-    else:
-        print('Warning: Inconsistent, revising CONTCAR_FS to CONTCAR_re...')
-        f = open('CONTCAR_re', 'w')
-        f.writelines(qian_zhui)
-        file_fs = open(fs_file, 'r')
-        data = file_fs.readlines()
-        coord = data[9:9 + num]
-        index_list = []
-        for n, i in enumerate(IS):
-            if n in ts_atoms:
-                f.write(coord[n])
-                index_list.append(n)
-            else:
-                all_dist = [np.linalg.norm(i - j) for j in FS]
-                ind = np.argmin(all_dist)
-                nn = 1
-                while True:
-                    if ind in index_list:
-                        ind = np.argsort(np.array(all_dist))[nn]
-                        nn += 1
-                    else:
-                        break
-                f.write(coord[ind])
-                index_list.append(ind)
-        f.close()
+    new_data = []
+    for inx in index:
+        new_data.append(data[9:][inx])
+    # rewrite
+    f = open('CONTCAR_rec', 'w')
+    f.writelines(qian_zhui)
+    f.writelines(new_data)
+    f.close()
 
 
 if __name__ == '__main__':
@@ -85,8 +103,6 @@ if __name__ == '__main__':
     contcar_is = sys.argv[1]
     contcar_fs = sys.argv[2]
     ts_atoms = sys.argv[3:]
-    compare_data(contcar_is,
-                 contcar_fs,
-                 ts_atoms=ts_atoms,
-                 threshold_value=0.2)
-
+    ts_atoms = [int(i) for i in ts_atoms]
+    ind = get_index(contcar_is, contcar_fs, ts_atoms)
+    generate_CONT(contcar_fs, ind)
